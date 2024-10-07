7901f14d51d35f06e0e46feb829ad0220ffea9da
BlablaTara <tara0561@stud.kea.dk>
2024-10-07 18:16:30 +0200

    activityService klasse opdateret

--- src/main/java/com/example/actionproeve/services/ActivityService.java
+++ src/main/java/com/example/actionproeve/services/ActivityService.java
@@ -3,7 +3,6 @@ package com.example.actionproeve.services;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -19,10 +18,8 @@ public class ActivityService {
 
     public ActivityService(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
-
-
-
     }
+
 
     public void saveActivity(Activity activity) {
         try {
@@ -54,44 +51,41 @@ public class ActivityService {
         }
     }
 
+
     /*
-     * public List<Activity> getAllActivities() {
-     * return activityRepository.findAll();
-     * }
-     * 
      * public Activity getActivityById(Long id) {
      * return activityRepository.findById(id).orElse(null);
      * }
-     * 
+     *
      * public Activity saveActivity(Activity activity) {
      * return activityRepository.save(activity);
      * }
-     * 
+     *
      * public void deleteActivity(Long id) {
      * activityRepository.deleteById(id);
      * }
-     * 
+     *
      * // Method to save hardcoded activities
      * public void saveHardcodedActivities() {
      * List<Activity> activities = new ArrayList<>();
-     * 
+     *
      * // Create hardcoded activities
      * Activity activity1 = new Activity();
      * activity1.setName("Running");
      * activity1.setTimes(List.of("07:00", "18:00")); // Example times
      * activity1.setDescription("A morning run to start the day.");
      * activity1.setImageUrl("http://example.com/running.jpg");
-     * 
+     *
      * Activity activity2 = new Activity();
      * activity2.setName("Swimming");
      * activity2.setTimes(List.of("10:00", "16:00")); // Example times
      * activity2.setDescription("Swimming for relaxation and fitness.");
      * activity2.setImageUrl("http://example.com/swimming.jpg");
-     * 
+     *
      * // Add activities to the list
      * activities.add(activity1);
      * activities.add(activity2);
-     * 
+     *
      * // Save all activities to the database
      * activityRepository.saveAll(activities);
      * }
