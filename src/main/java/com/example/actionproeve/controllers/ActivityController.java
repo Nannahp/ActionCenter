dc34a438072aa3a0550859f485facd85028bc81b
BlablaTara <tara0561@stud.kea.dk>
2024-10-07 18:13:08 +0200

    activityController opdateret

--- src/main/java/com/example/actionproeve/controllers/ActivityController.java
+++ src/main/java/com/example/actionproeve/controllers/ActivityController.java
@@ -3,14 +3,13 @@ package com.example.actionproeve.controllers;
 import com.example.actionproeve.models.Activity;
 import com.example.actionproeve.services.ActivityService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -19,30 +18,17 @@ import java.nio.file.Paths;
 
 // Rest - retunerer json-data.
 @RestController
-// Siger at denne tillader min vue ap adgang til denne backend, men min back-end
-// kører jo på 8080.
-@CrossOrigin(origins = "http://localhost:5173")
+// Siger at denne tillader min vue ap adgang til denne backend, men min back-end kører jo på 8080.
+@CrossOrigin (origins = "http://localhost:5173")
 
 public class ActivityController {
-    @Autowired
     private ActivityService activityService;
 
-    @GetMapping("/activity")
-    public Activity getActivity() {
-        System.out.println("sucess");
-        return new Activity();
+    // I stedet for autowired, så injecte igennem konstruktøren. Mere rigtigt - har jeg hørt.
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
     }
 
-    @PostMapping("/add-activity")
-    public ResponseEntity<?> saveActivity(@RequestBody Activity activity) {
-        System.out.println(activity.getName());
-        System.out.println(activity.getDescription());
-        System.out.println("Received activity: " + activity);
-        activityService.saveActivity(activity);
-
-        return ResponseEntity.ok().build();
-
-    }
 
     @GetMapping("/activities")
     public ResponseEntity<String> getActivities() throws IOException {
@@ -51,18 +37,29 @@ public class ActivityController {
         return ResponseEntity.ok(new String(jsonData));
     }
 
+    @PostMapping("/add-activity")
+    public ResponseEntity<?> saveActivity(@RequestBody Activity activity) {
+        System.out.println(activity.getActivityName());
+        System.out.println(activity.getInformation());
+        System.out.println("Received activity: " + activity);
+        activityService.saveActivity(activity);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+
     /*
-     * 
+     *
      * @GetMapping("/get-activities")
      * public List<Activity> getAllActivities() {
      * return activityService.getAllActivities();
      * }
-     * 
+     *
      * @PostMapping("/save-activities") // New endpoint to save hardcoded activities
      * public String saveActivities() {
      * activityService.saveHardcodedActivities();
      * return "Hardcoded activities saved!";
      * }
      */
-
 }
