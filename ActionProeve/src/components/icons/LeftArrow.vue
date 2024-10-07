f8b94d994069c695f0b77f397d0ae7583a051c57
emma673r <90177139+emma673r@users.noreply.github.com>
2024-10-07 11:39:47 +0200

    arrow icons componenter

---
+++ ActionProeve/src/components/icons/LeftArrow.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BaseImage from '../BaseImage.vue'
+import arrow from '../../assets/left-arrow.png'
+</script>
+
+<template>
+  <div class="logo">
+    <BaseImage :src="arrow" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.arrow {
+  width: 150px;
+}
+</style>
