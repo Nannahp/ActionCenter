8dc5e3ce6bb0def93d66f80ad8cf7994087cd6a0
Emma Pasquer <emma673r@stud.kea.dk>
2024-10-10 16:45:40 +0200

    done

--- ActionProeve/src/components/Activity.vue
+++ ActionProeve/src/components/Activity.vue
@@ -4,19 +4,19 @@ import missingImage from '../assets/missingpicture.png'
 
 const props = defineProps<{
   activity: {
-    activityName: string,
-    durations: string[],
-    information: string,
-    price: number,
-    minPeople: number,
-    maxPeople: number,
+    activityName: string
+    durations: string[]
+    information: string
+    price: number
+    minPeople: number
+    maxPeople: number
     image: string
-  },
+  }
   isReversed: boolean
-}>();
+}>()
 
-//Tjekker om der er et image i jsonfilen, og ellers bruger den det lokale billede.
-const imageMissing = props.activity.image || missingImage
+// //Tjekker om der er et image i jsonfilen, og ellers bruger den det lokale billede.
+// const imageMissing = props.activity.image || missingImage
 </script>
 
 <template>
@@ -24,21 +24,21 @@ const imageMissing = props.activity.image || missingImage
     <div class="activity-details">
       <h2>{{ props.activity.activityName }}</h2>
 
-      <p class="info-line">Choose between: </p>
+      <p class="info-line">Choose between:</p>
       <p>{{ props.activity.durations.join(', ') }}</p>
 
-      <p class="info-line">Info: </p>
+      <p class="info-line">Info:</p>
       <p>{{ props.activity.information }}</p>
 
-      <p class="info-line">Price: </p>
+      <p class="info-line">Price:</p>
       <p>{{ props.activity.price }} DKK</p>
 
-      <p class="info-line">Participants: </p>
+      <p class="info-line">Participants:</p>
       <p>Min: {{ props.activity.minPeople }} | Max: {{ props.activity.maxPeople }}</p>
     </div>
 
-    <div class="activity-image">
-      <img class="image" :src="imageMissing" :alt="props.activity.activityName + ' Image'" />
+    <div class="activity-image image" :style="{ backgroundImage: `url(${props.activity.image})` }">
+      <!-- <div class="image" :alt="props.activity.activityName + ' Image'"></div> -->
     </div>
   </div>
 </template>
@@ -46,43 +46,51 @@ const imageMissing = props.activity.image || missingImage
 <style scoped>
 .activity {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: flex-start;
   margin-bottom: 150px;
   margin-top: 40px;
-  padding: 20px;
-
+  height: 100%;
+  padding: 0 50px;
 }
 
 .activity.reverse {
   flex-direction: row-reverse;
 }
 
-.activity-details{
+.activity-details {
   flex: 1;
-  margin-left: 120px;
-  max-width: 600px;
+  max-width: 40vw;
 }
+
 .activity.reverse activity-details {
   margin-left: 0px;
-  margin-right: 150px;
+  padding-right: 150px;
 }
 
 .activity-image {
   flex: 1;
-  margin-left: 20px;
   text-align: center;
 }
+
+.activity-image {
+  flex: 1;
+  text-align: center;
+  height: 400px;
+  width: 40vw;
+  background-size: cover;
+  background-position: center;
+}
 .activity.reverse .activity-image {
-  margin-left: 50px;
-  margin-right: 0; /* Juster margin for billedet, når rækkefølgen ændres */
+  margin-right: 0;
 }
 
 .image {
-  max-width: 400px;
-  height: auto;
+  max-width: 45vw;
+  max-height: 400px;
   transition: transform 0.3s ease;
 }
+
 .image:hover {
   transform: scale(1.1);
 }
@@ -98,6 +106,4 @@ p {
   margin-top: 0;
   line-height: 1.6;
 }
-
-
 </style>
--- ActionProeve/src/views/HomePageView.vue
+++ ActionProeve/src/views/HomePageView.vue
@@ -1,72 +1,70 @@
 <script setup lang="ts">
 import Activity from '../components/Activity.vue'
 //import activitiesData from '@/assets/activities1.json'
-import
-  AdventureHeader from "@/components/AdventureHeader.vue";
-import {nextTick, onMounted, ref} from "vue";
-import axios from "axios";
+import AdventureHeader from '@/components/AdventureHeader.vue'
+import { nextTick, onMounted, ref } from 'vue'
+import axios from 'axios'
 import '@/assets/IndexMain.css'
-import FrontPageCarousel from "@/components/FrontPageCarousel.vue";
+import FrontPageCarousel from '@/components/FrontPageCarousel.vue'
 
 // Definerrer type for Activity
 interface ActivityType {
-  activityName: string;
-  durations: string[];
-  information: string;
-  price: number;
-  minPeople: number;
-  maxPeople: number;
-  image: string;
+  activityName: string
+  durations: string[]
+  information: string
+  price: number
+  minPeople: number
+  maxPeople: number
+  image: string
 }
 
 const activities = ref<ActivityType[]>([])
 
 onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:8080/activities');
-    activities.value = response.data;
+    const response = await axios.get('http://localhost:8080/activities')
+    activities.value = response.data
 
-    await nextTick();
+    await nextTick()
 
-    const activityElements = document.querySelectorAll(".activity");
+    const activityElements = document.querySelectorAll('.activity')
 
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add("slide-in");
+          entry.target.classList.add('slide-in')
         } else {
-          entry.target.classList.remove("slide-in");
+          entry.target.classList.remove('slide-in')
         }
-      });
-    });
+      })
+    })
 
-    activityElements.forEach(activity => {
-      observer.observe(activity);
-    });
-
+    activityElements.forEach((activity) => {
+      observer.observe(activity)
+    })
   } catch (error) {
-    console.error('Fejl under hentning af aktiviteter: ', error);
+    console.error('Fejl under hentning af aktiviteter: ', error)
   }
-});
-
-
+})
 </script>
 
 <template>
   <AdventureHeader />
 
   <div class="carousel">
-  <FrontPageCarousel />
+    <FrontPageCarousel />
   </div>
 
   <div id="{{activityName.toLowerCase()}}" class="activities-container">
     <h1>💥 Activities 💥</h1>
     <div class="activities-list">
-      <Activity class="activity"
-                v-for="(activity, index) in activities"
-                :key="index"
-                :activity="activity"
-                :isReversed="index % 2 === 1" :class="{'activity': true, 'slide-in-right': index % 2 === 1}"
+      <Activity
+        class="activity"
+        v-for="(activity, index) in activities"
+        :key="index"
+        :activity="activity"
+        :isReversed="index % 2 === 1"
+        :class="{ activity: true, 'slide-in-right': index % 2 === 1 }"
       />
     </div>
   </div>
@@ -74,17 +72,14 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-
 @media (min-width: 1024px) {
   header {
     display: flex;
-
 
     place-items: center;
     width: 100%;
   }
 }
-
 
 .activities-container {
   display: flex;
@@ -94,19 +89,17 @@ onMounted(async () => {
 
 .activity {
   opacity: 0;
-  transform: translateX(-100px);
+  transform: translateX(-250px);
   transition: all 0.5s ease-in-out;
 }
 
 .activity.slide-in-right {
   opacity: 1;
-  transform: translateX(100px); /* Slider fra højre */
+  transform: translateX(250px);
 }
 
 .activity.slide-in {
   opacity: 1;
   transform: translateX(0);
 }
-
-
 </style>
